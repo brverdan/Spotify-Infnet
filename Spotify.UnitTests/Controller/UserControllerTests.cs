65c9b73557f84fd6bbbc7f4631e0a7a24d7f9ea9
Bruno Verdan <bruno.verdan@al.infnet.edu.br>
2023-12-10 15:22:00 -0300

    feat: Criação teste de unidade da controller

---
+++ Spotify.UnitTests/Controller/UserControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Spotify.API.Controllers;
+using Spotify.Application.Interfaces;
+using Spotify.Domain.Accounts.Aggregates;
+using Spotify.Domain.Accounts.ValueObjects;
+
+namespace Spotify.UnitTests.Controller;
+
+[Trait(nameof(UserController), "")]
+public class UserControllerTests
+{
+    [Fact]
+    public void MustGetUserByIdSucess()
+    {
+        // Arrange
+        var userExpected = new User
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test name",
+            Email = "Test email",
+            Cpf = new CPF { Numero = "80549664076" },
+            CreditCards = new List<CreditCard>(),
+            Playlists = new List<Playlist>(),
+            Subscriptions = new List<Subscription>(),
+            Favorites = new List<Favorite>()
+        };
+         
+        var userService = new Mock<IUserService>();
+
+        userService.Setup(x => x.GetUserById(It.IsAny<Guid>())).Returns(userExpected);
+
+        var instancia = new UserController(userService.Object);
+        
+        // Act
+        var result = instancia.GetUserById(Guid.NewGuid());
+
+        // Assert
+        result.Should().BeOfType<OkObjectResult>();
+
+        result.As<OkObjectResult>().Value.Should().NotBeNull();
+        result.As<OkObjectResult>().Value.Should().BeEquivalentTo(userExpected);
+    }
+
+    [Fact]
+    public void MustGetUserByIdNotFoundUser()
+    {
+        // Arrange
+        var userService = new Mock<IUserService>();
+
+        userService.Setup(x => x.GetUserById(It.IsAny<Guid>()));
+
+        var instancia = new UserController(userService.Object);
+
+        // Act
+        var result = instancia.GetUserById(Guid.NewGuid());
+
+        // Assert
+        result.Should().BeOfType<NotFoundResult>();
+    }
+}
